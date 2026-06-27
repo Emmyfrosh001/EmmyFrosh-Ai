09e261933bb93f61e215b4283dadb9290e3ff32a
Emmyfrosh001 <elijahchristopher343@gmail.com>
2026-06-27 09:51:44 +0100

    Add initial implementation of bot management in index.js

---
+++ index.js
@@ -0,0 +1,170 @@
+function _0x5306(_0x476d53, _0x8540da) {
+    _0x476d53 = _0x476d53 - (0xfb * -0xd + -0x16e0 + -0x911 * -0x4);
+    const _0x537389 = _0x3519();
+    let _0x5dfd71 = _0x537389[_0x476d53];
+    return _0x5dfd71;
+}
+const _0x338973 = _0x5306;
+function _0x3519() {
+    const _0x3d4621 = [
+        '1311546ZmuUIV',
+        'exit',
+        '7VxhnkL',
+        'lgUIT',
+        'ot\x20started',
+        '647140ZucADx',
+        '326029SjQKTb',
+        'memory_rep',
+        'removeAllL',
+        'pid',
+        'rocess\x20exi',
+        '[SYSTEM]\x20R',
+        'black',
+        'bgRed',
+        'bgBlue',
+        'rss',
+        'PZIbc',
+        '4fvaUPo',
+        'type',
+        'M\x20exceeded',
+        'th\x20code\x20',
+        'MB.\x20Restar',
+        'child_proc',
+        '2081545XLFyMy',
+        '120MfiKCs',
+        'message',
+        'ort',
+        '[SYSTEM]\x20B',
+        'bgYellow',
+        'pVbls',
+        './main.js',
+        'bot\x20in\x20',
+        '110qiZldr',
+        's...',
+        '2514cFQagz',
+        'close',
+        'send',
+        'get_memory',
+        'yellow',
+        '[ALERT]\x20RA',
+        'd\x20bot\x20proc',
+        'chalk',
+        'hVRJu',
+        'ot\x20process',
+        'lqbeo',
+        '[SYSTEM]\x20S',
+        'ryDfv',
+        'topping\x20ol',
+        '185466lFYqIs',
+        'PXonF',
+        'estarting\x20',
+        'bgGreen',
+        'mDVtF',
+        'voSui',
+        'ess...',
+        'log',
+        '270ddkyYc',
+        'dFvKy',
+        'dSprp',
+        '128416VIykDO',
+        'kill',
+        '29icwfsq',
+        'ting\x20bot..'
+    ];
+    _0x3519 = function () {
+        return _0x3d4621;
+    };
+    return _0x3519();
+}
+(function (_0x15fb1f, _0x197715) {
+    const _0x3a6884 = _0x5306, _0x598252 = _0x15fb1f();
+    while (!![]) {
+        try {
+            const _0xc5dfe5 = parseInt(_0x3a6884(0xd4)) / (0x1f5d + 0x2 * 0x11ea + -0x4330) * (-parseInt(_0x3a6884(0xb9)) / (-0x1 * -0x3fa + -0xeba + 0xac2)) + parseInt(_0x3a6884(0xd6)) / (-0x1 * 0x244c + -0x1 * 0x13ed + 0x383c) * (parseInt(_0x3a6884(0xa8)) / (0x5af + 0x168 + -0x713 * 0x1)) + parseInt(_0x3a6884(0xae)) / (-0x222c + 0x1 * 0x1a4b + 0x7e6) + -parseInt(_0x3a6884(0xc7)) / (-0x7bf + 0x13 * -0x52 + -0x1 * -0xddb) * (parseInt(_0x3a6884(0xd8)) / (-0xabc + -0x1 * -0x2059 + -0x1596)) + -parseInt(_0x3a6884(0xd2)) / (0x578 + -0x2 * -0xf44 + -0x8fe * 0x4) * (-parseInt(_0x3a6884(0xcf)) / (0x1071 + -0x1007 * -0x1 + 0x1b5 * -0x13)) + -parseInt(_0x3a6884(0xb7)) / (-0x11c2 + 0x13 * -0x28 + 0x14c4) * (parseInt(_0x3a6884(0xdc)) / (0x1d84 + -0x1 * 0x9cd + -0x13ac)) + -parseInt(_0x3a6884(0xaf)) / (0x1d39 + 0x3f * 0x73 + -0x397a) * (parseInt(_0x3a6884(0xdb)) / (0x14d7 + -0x29 * -0x6d + -0x263f));
+            if (_0xc5dfe5 === _0x197715)
+                break;
+            else
+                _0x598252['push'](_0x598252['shift']());
+        } catch (_0x1ba642) {
+            _0x598252['push'](_0x598252['shift']());
+        }
+    }
+}(_0x3519, -0x64 * -0xcc2 + 0x81c67 + -0x6525d));
+const {fork} = require(_0x338973(0xad) + 'ess'), chalk = require(_0x338973(0xc0)), MAX_MEMORY_MB = -0xbb9 * -0x3 + 0x22 * -0xb + 0x1e95 * -0x1, MEMORY_CHECK_INTERVAL_MS = -0xe21 * 0x8 + 0x1f31 + 0x8c6f, RESTART_DELAY = 0x65 * -0x3b + -0x2643 + 0x9 * 0x902;
+let child = null, memoryCheckTimer = null, restarting = ![];
+function start() {
+    const _0x4d946f = _0x338973, _0x4fff10 = {
+            'dSprp': _0x4d946f(0xdd) + _0x4d946f(0xb1),
+            'xpGBS': function (_0x1f1a34, _0x28a012) {
+                return _0x1f1a34 * _0x28a012;
+            },
+            'lgUIT': 'child\x20to\x20p' + 'arent\x20=>',
+            'lqbeo': function (_0x3d902b) {
+                return _0x3d902b();
+            },
+            'PZIbc': _0x4d946f(0xbc),
+            'PXonF': _0x4d946f(0xc4) + _0x4d946f(0xc6) + _0x4d946f(0xbf) + _0x4d946f(0xcd),
+            'URZWD': 'SIGKILL',
+            'pVbls': _0x4d946f(0xb5),
+            'ryDfv': _0x4d946f(0xba),
+            'dFvKy': _0x4d946f(0xd7),
+            'voSui': function (_0x1de2fb, _0x932afb) {
+                return _0x1de2fb(_0x932afb);
+            }
+        };
+    if (restarting)
+        return;
+    restarting = !![];
+    if (child) {
+        console['log'](chalk[_0x4d946f(0xe2)](chalk[_0x4d946f(0xb3)](_0x4fff10[_0x4d946f(0xc8)])));
+        try {
+            child[_0x4d946f(0xde) + 'isteners'](), child[_0x4d946f(0xd3)](_0x4fff10['URZWD']);
+        } catch (_0x14d8b9) {
+            console[_0x4d946f(0xce)](_0x14d8b9);
+        }
+    }
+    child = fork(_0x4fff10[_0x4d946f(0xb4)]), console['log'](chalk[_0x4d946f(0xe2)](chalk[_0x4d946f(0xca)](_0x4d946f(0xb2) + _0x4d946f(0xda) + '\x20successfu' + 'lly\x20(PID:\x20' + child[_0x4d946f(0xdf)] + ')'))), restarting = ![], child['on'](_0x4d946f(0xb0), _0x11e72c => {
+        const _0x4623c2 = _0x4d946f;
+        if (!_0x11e72c)
+            return;
+        if (_0x11e72c[_0x4623c2(0xa9)] === _0x4fff10[_0x4623c2(0xd1)] && _0x11e72c[_0x4623c2(0xa6)]) {
+            const _0x84d03b = _0x11e72c[_0x4623c2(0xa6)] / _0x4fff10['xpGBS'](0x40 * -0x92 + 0x1 * -0x1d27 + -0xb * -0x655, 0x158 * -0x16 + -0x328 * -0xb + -0x128);
+            _0x84d03b > MAX_MEMORY_MB && (console[_0x4623c2(0xce)](chalk[_0x4623c2(0xe2)](chalk['bgRed'](_0x4623c2(0xbe) + _0x4623c2(0xaa) + '\x20' + MAX_MEMORY_MB + (_0x4623c2(0xac) + _0x4623c2(0xd5) + '.')))), restartBot());
+        } else
+            console[_0x4623c2(0xce)](_0x4fff10[_0x4623c2(0xd9)], _0x11e72c);
+    }), child['on'](_0x4fff10[_0x4d946f(0xc5)], _0x1642ae => {
+        const _0x6e389e = _0x4d946f;
+        console['log'](chalk[_0x6e389e(0xe2)](chalk[_0x6e389e(0xe3)](_0x6e389e(0xb2) + _0x6e389e(0xc2) + '\x20closed\x20wi' + _0x6e389e(0xab) + _0x1642ae))), _0x4fff10[_0x6e389e(0xc3)](restartBot);
+    }), child['on'](_0x4fff10[_0x4d946f(0xd0)], _0xebd180 => {
+        const _0x3bcc33 = _0x4d946f;
+        console[_0x3bcc33(0xce)](chalk[_0x3bcc33(0xbd)]('[SYSTEM]\x20P' + _0x3bcc33(0xe0) + 'ted\x20with\x20c' + 'ode\x20' + _0xebd180));
+    }), memoryCheckTimer && _0x4fff10[_0x4d946f(0xcc)](clearInterval, memoryCheckTimer), memoryCheckTimer = setInterval(() => {
+        const _0xe1198c = _0x4d946f;
+        if (!child)
+            return;
+        if (child['connected']) {
+            const _0x541e8b = {};
+            _0x541e8b[_0xe1198c(0xa9)] = _0x4fff10[_0xe1198c(0xa7)], child[_0xe1198c(0xbb)](_0x541e8b);
+        }
+    }, MEMORY_CHECK_INTERVAL_MS);
+}
+function restartBot() {
+    const _0x25df4d = _0x338973, _0x21303b = {
+            'hVRJu': function (_0x69a46c) {
+                return _0x69a46c();
+            },
+            'Tcpla': function (_0x535be8, _0x174e11) {
+                return _0x535be8 / _0x174e11;
+            },
+            'mDVtF': function (_0x27a10f, _0x4c3006) {
+                return _0x27a10f(_0x4c3006);
+            }
+        };
+    if (restarting)
+        return;
+    restarting = !![], console[_0x25df4d(0xce)](chalk['black'](chalk[_0x25df4d(0xa5)](_0x25df4d(0xe1) + _0x25df4d(0xc9) + _0x25df4d(0xb6) + _0x21303b['Tcpla'](RESTART_DELAY, -0xbc7 + -0x57 * -0x11 + -0x9e8 * -0x1) + _0x25df4d(0xb8)))), memoryCheckTimer && _0x21303b[_0x25df4d(0xcb)](clearInterval, memoryCheckTimer), setTimeout(() => {
+        const _0x51346f = _0x25df4d;
+        restarting = ![], _0x21303b[_0x51346f(0xc1)](start);
+    }, RESTART_DELAY);
+}
+start();
