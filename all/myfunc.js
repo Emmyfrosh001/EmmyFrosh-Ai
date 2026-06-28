478bc2e29323d141e230e28419c8e240922b4491
Emmyfrosh001 <elijahchristopher343@gmail.com>
2026-06-28 02:50:52 +0100

    Add myfunc.js with various utility functions

---
+++ all/myfunc.js
@@ -0,0 +1,254 @@
+const _0x1b4f = [
+  '\x69\x6d\x61\x67\x65\x4d\x65\x73\x73\x61\x67\x65', '\x76\x69\x64\x65\x6f\x4d\x65\x73\x73\x61\x67\x65', 
+  '\x61\x75\x64\x69\x6f\x4d\x65\x73\x73\x61\x67\x65', '\x75\x6e\x6b\x6e\x6f\x7d\x6e', '\x76\x69\x65\x77\x4f\x6e\x63\x65\x4d\x65\x73\x73\x61\x67\x65',
+  '\x6c\x69\x73\x74\x52\x65\x73\x70\x6f\x6e\x73\x65\x4d\x65\x73\x73\x61\x67\x65', '\x73\x69\x6e\x67\x6c\x65\x53\x65\x6c\x65\x63\x74\x52\x65\x70\x6c\x79', 
+  '\x73\x65\x6c\x65\x63\x74\x65\x64\x52\x6f\x77\x49\x64', '\x62\x75\x74\x74\x6f\x6e\x73\x52\x65\x73\x70\x6f\x6e\x73\x65\x4d\x65\x73\x73\x61\x67\x65',
+  '\x73\x65\x6c\x65\x63\x74\x65\x64\x42\x75\x74\x74\x6f\x6e\x49\x64', '\x63\x6f\x6e\x74\x65\x78\x74\x49\x6e\x66\x6f', '\x71\x75\x6f\x74\x65\x64\x4d\x65\x73\x73\x61\x67\x65', 
+  '\x70\x72\x6f\x64\x75\x63\x74\x4d\x65\x73\x73\x61\x67\x65', '\x73\x74\x61\x6e\x7a\x61\x49\x64', '\x72\x65\x6d\x6f\x74\x65\x4a\x69\x64', 
+  '\x70\x61\x72\x74\x69\x63\x69\x70\x61\x6e\x74', '\x66\x61\x6b\x65\x4f\x62\x6a', '\x66\x72\x6f\x6d\x4f\x62\x6a\x65\x63\x74', '\x64\x65\x6c\x65\x74\x65', 
+  '\x63\x6f\x70\x79\x4e\x46\x6f\x72\x77\x61\x72\x64', '\x64\x6f\x77\x6e\x6c\x6f\x61\x64', '\x75\x72\x6c', '\x73\x65\x6e\x64\x4d\x65\x64\x69\x61', 
+  '\x73\x65\x6e\x64\x54\x65\x78\x74', '\x74\x6f\x4f\x62\x6a\x65\x63\x74', '\x61\x70\x70\x65\x6e\x54\x65\x78\x74\x4d\x65\x73\x73\x61\x67\x65',
+  '\x67\x65\x6e\x65\x72\x61\x74\x65\x57\x41\x4d\x65\x73\x73\x61\x67\x65', '\x61\x72\x65\x4a\x69\x64\x73\x53\x61\x6d\x65\x55\x73\x65\x72', 
+  '\x6d\x65\x73\x73\x61\x67\x65\x73\x2e\x75\x70\x73\x65\x72\x74', '\x73\x6d\x73\x67\x20\x65\x72\x72\x6f\x72\x3a', '\x64\x61\x74\x61', '\x70\x6f\x73\x74', 
+  '\x68\x74\x74\x70\x73\x3a\x2f\x2f\x73\x68\x72\x74\x72\x6c\x2e\x76\x65\x72\x63\x65\x6c\x2e\x61\x70\x70\x2f', '\x73\x68\x6f\x72\x74\x55\x72\x6c', 
+  '\x4a\x45\x44\x45\x43', '\x72\x65\x6e\x64\x65\x72', '\x47\x45\x54', '\x63\x6f\x6e\x74\x65\x6e\x74\x2d\x6c\x65\x6e\x67\x74\x68', 
+  '\x4e\x6f\x20\x63\x6f\x6e\x74\x65\x6e\x74\x2d\x6c\x65\x6e\x67\x74\x68\x20\x68\x65\x61\x64\x65\x72', '\x55\x6e\x73\x75\x70\x70\x6f\x72\x74\x65\x64\x20\x70\x61\x74\x68\x20\x74\x79\x70\x65', 
+  '\x57\x65\x62\x4d\x65\x73\x73\x61\x67\x65\x49\x6e\x66\x6f', '\x65\x70\x68\x65\x6d\x65\x72\x61\x6c\x4d\x65\x73\x73\x61\x67\x65', 
+  '\x6b\x65\x79', '\x6d\x65\x73\x73\x61\x67\x65', '\x69\x64', '\x63\x68\x61\x74', '\x66\x72\x6f\x6d\x4d\x65', '\x69\x73\x42\x61\x69\x6c\x65\x79\x73', 
+  '\x69\x73\x47\x72\x6f\x75\x70', '\x69\x73\x4c\x69\x64', '\x69\x73\x55\x73\x65\x72', '\x73\x65\x6e\x64\x65\x72', '\x6e\x6f\x72\x6d\x61\x6c\x69\x7a\x65\x64\x53\x65\x6e\x64\x65\x72', 
+  '\x6d\x74\x79\x70\x65', '\x6d\x73\x67', '\x62\x6f\x64\x79', '\x71\x75\x6f\x74\x65\x64', '\x64\x6f\x77\x6e\x6c\x6f\x61\x64\x4d\x65\x64\x69\x61\x4d\x65\x73\x73\x61\x67\x65', 
+  '\x63\x6f\x70\x79', '\x61\x70\x70\x65\x6e\x64', '\x76\x69\x65\x77\x4f\x6e\x63\x65\x4d\x65\x73\x73\x61\x67\x65\x56\x32', 
+  '\x76\x69\x65\x77\x4f\x6e\x63\x65\x4d\x65\x73\x73\x61\x67\x65\x56\x32\x45\x78\x74\x65\x6e\x73\x69\x6f\x6e', '\x42\x41\x45\x35', '\x40\x67\x2e\x75\x73', 
+  '\x40\x6c\x69\x64', '\x40\x73\x2e\x77\x68\x61\x74\x73\x61\x70\x70\x2e\x6e\x65\x74', '\x64\x65\x63\x6f\x64\x65\x4a\x69\x64', '\x75\x73\x65\x72', 
+  '\x72\x65\x70\x6c\x61\x63\x65', '\x67\x65\x74\x43\x6f\x6e\x74\x65\x6e\x74\x54\x79\x70\x65', '\x63\x6f\x6e\x76\x65\x72\x73\x61\x74\x69\x6f\x6e', 
+  '\x63\x61\x70\x7a\x69\x6f\x6e', '\x74\x65\x78\x74', '\x65\x78\x74\x65\x6e\x64\x65\x64\x54\x65\x78\x74\x4d\x65\x73\x73\x61\x67\x65', '\x6d\x65\x6e\x74\x69\x6f\x6e\x65\x64\x4a\x69\x64', 
+  '\x42\x79\x74\x65\x73', '\x20\x4b\x42', '\x20\x4d\x42', '\x20\x47\x42', '\x20\x54\x42', '\x20\x50\x42', '\x20\x45\x42', '\x30\x20\x42\x79\x74\x65\x73', 
+  '\x4b\x42', '\x4d\x42', '\x47\x42', '\x54\x42', '\x50\x42', '\x45\x42', '\x5a\x42', '\x59\x42', '\x6e\x65\x74\x73\x74\x61\x74', '\x73\x74\x61\x74\x73', 
+  '\x6e\x6f\x64\x65\x2d\x6f\x73\x2d\x75\x74\x69\x6c\x73', '\x69\x6e\x70\x75\x74\x42\x79\x74\x65\x73', '\x6f\x75\x74\x70\x75\x74\x42\x79\x74\x65\x73', 
+  '\x4d\x6f\x7a\x69\x6c\x6c\x61\x2f\x35\x2e\x30\x20\x28\x57\x69\x6e\x64\x6f\x77\x73\x20\x4e\x54\x20\x31\x30\x2e\x30\x3b\x20\x57\x69\x6e\x36\x34\x3b\x20\x78\x36\x34\x29\x20\x41\x70\x70\x6c\x65\x57\x65\x62\x4b\x69\x74\x2f\x35\x33\x37\x2e\x33\x36\x20\x28\x4b\x48\x54\x4d\x4c\x2c\x20\x6c\x69\x6b\x65\x20\x47\x65\x63\x6b\x6f\x29\x20\x43\x68\x72\x6f\x6d\x65\x2f\x39\x35\x2e\x30\x20\x53\x61\x66\x61\x72\x69\x2f\x35\x33\x37\x2e\x33\x36', 
+  '\x61\x72\x72\x61\x79\x62\x75\x66\x66\x65\x72', '\x68\x74\x74\x70\x73\x3f\x3a\x2f\x2f\x28\x77\x77\x77\x5c\x2e\x29\x3f\x5b\x2d\x61\x2d\x7a\x41\x2d\x5a\x30\x2d\x39\x40\x3a\x25\x2e\x5f\x2b\x7e\x23\x3d\x5d\x7b\x31\x2c\x32\x35\x36\x7d\x5c\x2e\x5b\x61\x2d\x7a\x41\x2d\x5a\x30\x2d\x39\x28\x29\x5d\x7b\x31\x2c\x36\x7d\x5c\x62\x28\x5b\x2d\x61\x2d\x7a\x41\x2d\x5a\x30\x2d\x39\x28\x29\x40\x3a\x25\x5f\x2b\x2e\x7e\x23\x3f\x26\x2f\x3d\x5d\x2a\x29', 
+  '\x64\x61\x79\x2c\x20', '\x64\x61\x79\x73\x2c\x20', '\x68\x6f\x75\x72\x2c\x20', '\x68\x6f\x75\x72\x73\x2c\x20', '\x6d\x69\x6e\x75\x74\x65\x2c\x20', 
+  '\x6d\x69\x6e\x75\x74\x65\x73\x2c\x20', '\x73\x65\x63\x6f\x6e\x64', '\x73\x65\x63\x6f\x6e\x64\x73', '\x30\x20\x73\x65\x63\x6f\x6e\x64\x73'
+];
+
+const _0x0c31 = function (_0x4810a4, _0x1b4f44) {
+  _0x4810a4 = _0x4810a4 - 0x0;
+  return _0x1b4f[_0x4810a4];
+};
+
+const { proto, generateWAMessage, getContentType, areJidsSameUser } = require('@whiskeysockets/baileys');
+const chalk = require("chalk");
+const axios = require("axios");
+const { sizeFormatter } = require("human-readable");
+
+exports.color = (t, c) => (!c ? chalk.green(t) : chalk.keyword(c)(t));
+
+exports.getGroupAdmins = (p = []) => {
+  const a = [];
+  for (const x of p) {
+    if (x?.admin === "\x73\x75\x70\x65\x72\x61\x64\x6d\x69\x6e" || x?.admin === "\x61\x64\x6d\x69\x6e") {
+      a.push(x.jid || x.id);
+    }
+  }
+  return a;
+};
+
+exports.h2k = (n) => {
+  const pf = ["", "\x20\x52\x69\x62\x75", "\x20\x4a\x75\x74\x61", "\x20\x4d\x69\x6c\x69\x61\x72", "\x20\x54\x72\x69\x6c\x69\x75\x6e", "\x20\x50", "\x20\x45"];
+  if (!Number.isFinite(n)) return n;
+  const t = (Math.log10(Math.abs(n)) / 3) | 0;
+  if (t === 0) return n;
+  return (n / Math.pow(10, t * 3)).toFixed(1).replace(/\.0$/, "") + (pf[t] || "");
+};
+
+exports.FileSize = (n) => {
+  const pf = [_0x0c31('0x4b'), _0x0c31('0x4c'), _0x0c31('0x4d'), _0x0c31('0x4e'), _0x0c31('0x4f'), _0x0c31('0x50'), _0x0c31('0x51')];
+  if (!Number.isFinite(n)) return n;
+  const t = (Math.log10(Math.abs(n)) / 3) | 0;
+  if (t === 0) return n;
+  return (n / Math.pow(10, t * 3)).toFixed(1).replace(/\.0$/, "") + (pf[t] || "");
+};
+
+exports.bytesToSize = (b, d = 2) => {
+  if (!Number.isFinite(b) || b <= 0) return _0x0c31('0x52');
+  const s = [_0x0c31('0x4b'), _0x0c31('0x53'), _0x0c31('0x54'), _0x0c31('0x55'), _0x0c31('0x56'), _0x0c31('0x57'), _0x0c31('0x58'), _0x0c31('0x59'), _0x0c31('0x5a')];
+  const i = Math.floor(Math.log(b) / Math.log(1024));
+  return parseFloat((b / Math.pow(1024, i)).toFixed(d < 0 ? 0 : d)) + " " + s[i];
+};
+
+exports.formatSize = (b) => {
+  if (!Number.isFinite(b) || b <= 0) return _0x0c31('0x52');
+  const s = [_0x0c31('0x4b'), _0x0c31('0x53'), _0x0c31('0x54'), _0x0c31('0x55'), _0x0c31('0x56')];
+  const i = Math.floor(Math.log(b) / Math.log(1024));
+  return (b / Math.pow(1024, i)).toFixed(2) + " " + s[i];
+};
+
+exports.checkBandwidth = async () => {
+  let ind = 0, out = 0;
+  const st = await require(_0x0c31('0x5d'))[_0x0c31('0x5b')][_0x0c31('0x5c')]();
+  for (const i of st) {
+    ind += parseInt(i[_0x0c31('0x5e')] || 0);
+    out += parseInt(i[_0x0c31('0x5f')] || 0);
+  }
+  return { download: exports.bytesToSize(ind), upload: exports.bytesToSize(out) };
+};
+
+exports.fetchJson = async (u, o) => {
+  try {
+    const r = await axios({ method: _0x0c31('0x24'), url: u, headers: { "User-Agent": _0x0c31('0x60') }, ...(o || {}) });
+    return r.data;
+  } catch (e) { return e; }
+};
+
+exports.getBuffer = async (u, o) => {
+  try {
+    const r = await axios({ method: _0x0c31('0x24'), url: u, headers: { DNT: 1, "Upgrade-Insecure-Request": 1 }, ...(o || {}), responseType: _0x0c31('0x61') });
+    return r.data;
+  } catch (e) { return e; }
+};
+
+exports.nganuin = exports.fetchJson;
+
+exports.getRandom = (e = "") => `${Math.floor(Math.random() * 10000)}${e}`;
+exports.pickRandom = (a = []) => a[Math.floor(Math.random() * a.length)];
+
+exports.isUrl = (u = "") => typeof u === "\x73\x74\x72\x69\x6e\x67" && new RegExp(_0x0c31('0x62'), 'gi').test(u);
+
+exports.jsonformat = (d) => {
+  try { return JSON.stringify(d, null, 2); } catch { return String(d); }
+};
+
+exports.runtime = function (s) {
+  s = Number(s) || 0;
+  const d = Math.floor(s / 86400), h = Math.floor((s % 86400) / 3600), m = Math.floor((s % 3600) / 60), sec = Math.floor(s % 60);
+  return (d > 0 ? d + (d === 1 ? _0x0c31('0x63') : _0x0c31('0x64')) : "") +
+         (h > 0 ? h + (h === 1 ? _0x0c31('0x65') : _0x0c31('0x66')) : "") +
+         (m > 0 ? m + (m === 1 ? _0x0c31('0x67') : _0x0c31('0x68')) : "") +
+         (sec > 0 ? sec + (sec === 1 ? _0x0c31('0x69') : _0x0c31('0x6a')) : "") || _0x0c31('0x6b');
+};
+
+exports.shorturl = async function (lu) {
+  try {
+    const r = await axios[_0x0c31('0x1f')](_0x0c31('0x20'), { url: lu });
+    return r?.[_0x0c31('0x1e')]?.[_0x0c31('0x1e')]?.[_0x0c31('0x21')] || lu;
+  } catch { return lu; }
+};
+
+exports.formatp = sizeFormatter({ std: _0x0c31('0x22'), decimalPlaces: 2, keepTrailingZeroes: false, render: (l, s) => `${l} ${s}\x42` });
+
+exports.sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+exports.getSizeMedia = (p) => new Promise((res, rej) => {
+  try {
+    if (typeof p === "\x73\x74\x72\x69\x6e\x67" && /^https?:\/\//i.test(p)) {
+      axios.get(p, { method: _0x0c31('0x24') }).then((r) => {
+        const len = parseInt(r.headers[_0x0c31('0x25')] || "0");
+        if (!isNaN(len)) res(exports.bytesToSize(len, 3));
+        else rej(new Error(_0x0c31('0x26')));
+      }).catch(rej);
+    } else if (Buffer.isBuffer(p)) {
+      res(exports.bytesToSize(Buffer.byteLength(p), 3));
+    } else { rej(new Error(_0x0c31('0x27'))); }
+  } catch (e) { rej(e); }
+});
+
+exports.smsg = (ptz = {}, m = null, store = null) => {
+  try {
+    if (!m || typeof m !== "\x6f\x62\x6a\x65\x63\x74") return {};
+    const M = proto[_0x0c31('0x28')];
+
+    while (m?.message?.[_0x0c31('0x29')]) m.message = m.message[_0x0c31('0x29')].message || {};
+    while (m?.message?.[_0x0c31('0x04')]) m.message = m.message[_0x0c31('0x04')].message || {};
+    while (m?.message?.[_0x0c31('0x3c')]) m.message = m.message[_0x0c31('0x3c')].message || {};
+    while (m?.message?.[_0x0c31('0x3d')]) m.message = m.message[_0x0c31('0x3d')].message || {};
+
+    const key = m.key || {};
+    const remote = key.remoteJid || "";
+    m.id = key.id || "";
+    m.chat = remote;
+    m.fromMe = !!key.fromMe;
+    m.isBaileys = m.id.startsWith(_0x0c31('0x3e')) && m.id.length === 16;
+    m.isGroup = remote.endsWith(_0x0c31('0x3f'));
+    m.isLid = remote.endsWith(_0x0c31('0x40'));
+    m.isUser = remote.endsWith(_0x0c31('0x41')) || m.isGroup || m.isLid;
+
+    const decode = typeof ptz[_0x0c31('0x42')] === "\x66\x75\x6e\x63\x74\x69\x6f\x6e" ? ptz[_0x0c31('0x42')] : (x) => x || "";
+    const part = (m.fromMe && ptz?.[_0x0c31('0x43')]?.id) || m.participant || key.participant || m.chat || "";
+    m.sender = decode(part);
+    if (m.isGroup) m.participant = decode(key.participant) || "";
+
+    m.normalizedSender = m.sender.endsWith(_0x0c31('0x40')) ? m.sender[_0x0c31('0x44')](_0x0c31('0x40'), _0x0c31('0x41')) : m.sender;
+
+    let mt = _0x0c31('0x03');
+    try { if (m.message) mt = getContentType(m.message) || _0x0c31('0x03'); } catch {}
+    m.mtype = mt;
+    m.msg = mt === _0x0c31('0x04') ? m.message[mt]?.message?.[getContentType(m.message[mt]?.message || {})] : m.message[mt];
+
+    m.body = m.message?.[_0x0c31('0x46')] || m.msg?.[_0x0c31('0x47')] || m.msg?.[_0x0c31('0x48')] || m.message?.[_0x0c31('0x49')]?.[_0x0c31('0x48')] || (m.mtype === _0x0c31('0x05') && m.msg?.[_0x0c31('0x06')]?.[_0x0c31('0x07')]) || (m.mtype === _0x0c31('0x08') && m.msg?.[_0x0c31('0x09')]) || m.text || "";
+
+    if (m.msg?.[_0x0c31('0x47')]) m.caption = m.msg[_0x0c31('0x47')];
+    m.mentionedJid = (m.msg?.[_0x0c31('0x0a')]?.[_0x0c31('0x4a')] || []).filter(Boolean);
+
+    const qRaw = m.msg?.[_0x0c31('0x0a')]?.[_0x0c31('0x0b')];
+    if (qRaw && typeof qRaw === "\x6f\x62\x6a\x65\x63\x74") {
+      let type = Object.keys(qRaw)[0], q = qRaw[type];
+      if (type === _0x0c31('0x0c') && q && typeof q === "\x6f\x62\x6a\x65\x63\x74") { type = Object.keys(q)[0]; q = q[type]; }
+      if (typeof q === "\x73\x74\x72\x69\x6e\x67") q = { text: q };
+
+      m.quoted = q || {};
+      m.quoted.mtype = type || _0x0c31('0x03');
+      m.quoted.id = m.msg?.[_0x0c31('0x0a')]?.[_0x0c31('0x0d')] || "";
+      m.quoted.chat = m.msg?.[_0x0c31('0x0a')]?.[_0x0c31('0x0e')] || m.chat || "";
+      m.quoted.isBaileys = m.quoted.id.startsWith(_0x0c31('0x3e')) && m.quoted.id.length === 16;
+      m.quoted.sender = decode(m.msg?.[_0x0c31('0x0a')]?.[_0x0c31('0x0f')]) || "";
+      m.quoted.fromMe = m.quoted.sender === decode(ptz?.[_0x0c31('0x43')]?.id || "");
+      m.quoted.text = m.quoted.text || m.quoted.caption || m.quoted.conversation || m.quoted.contentText || m.quoted.selectedDisplayText || m.quoted.title || "";
+      m.quoted.mentionedJid = (m.msg?.[_0x0c31('0x0a')]?.[_0x0c31('0x4a')] || []).filter(Boolean);
+
+      m.getQuotedMessage = async () => {
+        if (!m.quoted.id || !store) return false;
+        const qMsg = await store.loadMessage(m.chat, m.quoted.id, ptz).catch(() => null);
+        return qMsg ? exports.smsg(ptz, qMsg, store) : false;
+      };
+
+      const vM = (m.quoted[_0x0c31('0x10')] = proto.WebMessageInfo.fromObject({
+        key: { remoteJid: m.quoted.chat, fromMe: m.quoted.fromMe, id: m.quoted.id },
+        message: qRaw,
+        ...(m.isGroup ? { participant: m.quoted.sender } : {}),
+      }));
+
+      m.quoted[_0x0c31('0x12')] = () => ptz?.sendMessage && ptz.sendMessage(m.quoted.chat, { delete: vM.key });
+      m.quoted[_0x0c31('0x13')] = (jid, force = false, opts = {}) => ptz?.[_0x0c31('0x13')]?.(jid, vM, force, opts);
+      m.quoted[_0x0c31('0x14')] = () => ptz?.[_0x0c31('0x39')]?.(m.quoted);
+    }
+
+    if (m.msg?.[_0x0c31('0x15')] && typeof ptz.downloadMediaMessage === "\x66\x75\x6e\x63\x74\x69\x6f\x6e") {
+      m.download = () => ptz.downloadMediaMessage(m.msg);
+    }
+
+    m.text = m.body || m.msg?.text || m.msg?.caption || m.message?.[_0x0c31('0x46')] || m.message?.[_0x0c31('0x49')]?.text || "";
+
+    m.reply = (text, chatId = m.chat, options = {}) => {
+      if (!ptz) return;
+      if (Buffer.isBuffer(text)) return ptz[_0x0c31('0x16')]?.(chatId, text, "\x66\x69\x6c\x65", "", m, { ...options });
+      return ptz[_0x0c31('0x17')]?.(chatId, String(text), m, { ...options });
+    };
+
+    m[_0x0c31('0x3a')] = () => exports.smsg(ptz, M.fromObject(M[_0x0c31('0x18')](m)), store);
+    m[_0x0c31('0x13')] = (jid = m.chat, force = false, opts = {}) => ptz.copyNForward?.(jid, m, force, opts);
+
+    ptz[_0x0c31('0x19')] = async (text, chatUp = {}) => {
+      const msgs = await generateWAMessage(m.chat, { text: String(text), mentions: m.mentionedJid }, { userJid: ptz?.user?.id, quoted: m.quoted?.[_0x0c31('0x10')] });
+      msgs.key.fromMe = areJidsSameUser(m.sender, ptz?.user?.id);
+      if (m?.key?.id) msgs.key.id = m.key.id;
+      msgs.pushName = m.pushName;
+      if (m.isGroup) msgs.participant = m.sender;
+      ptz?.ev?.emit?.(_0x0c31('0x1b'), { ...chatUp, messages: [proto.WebMessageInfo.fromObject(msgs)], type: _0x0c31('0x3b') });
+    };
+
+    return m;
+  } catch (e) {
+    console.error(_0x0c31('0x1c'), e);
+    return {};
+  }
+};
